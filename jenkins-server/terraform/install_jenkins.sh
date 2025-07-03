a833b81fc8fdff863900b9ee39ba9398ac3fa29e
dominik-mzg <dominikmazgaj1234@gmail.com>
2025-07-03 21:09:12 +0200

    Create install_jenkins.sh

---
+++ jenkins-server/terraform/install_jenkins.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Aktualizujemy system
+sudo apt update -y
+
+# Instalujemy Javę (wymaganą przez Jenkinsa)
+sudo apt install -y openjdk-17-jre
+
+# Instalujemy Jenkinsa
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y jenkins
+
+# Instalujemy Dockera, kubectl i kops
+sudo apt install -y docker.io
+sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+
+# Uruchamiamy Jenkinsa
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
